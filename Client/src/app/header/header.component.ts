0ba81b40b0d3b29cc9f37ab027e26d561e26707f
rvv <rolandoavv@gmail.com>
2021-05-31 20:23:41 +0100

    Second commit

--- Client/src/app/header/header.component.ts
+++ Client/src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../shared/user.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
--- Client/src/app/user/sign-in/sign-in.component.ts
+++ Client/src/app/user/sign-in/sign-in.component.ts
@@ -7,7 +7,7 @@ import { UserService } from '../../shared/user.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
 
